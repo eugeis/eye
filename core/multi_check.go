cc221485fab0ad37720a601e3b63ab5c1892a32b
Eugen Eisler <eoeisler@gmail.com>
2017-03-30 11:21:27 +0200

    Add command support

---
+++ core/multi_check.go
@@ -0,0 +1,59 @@
+package core
+
+import (
+	"errors"
+	"regexp"
+)
+
+type QueryResultInfo struct {
+	result QueryResult
+	err    error
+}
+
+type MultiCheck struct {
+	info        string
+	query       string
+	pattern     *regexp.Regexp
+	checks      []Check
+	onlyRunning bool
+	validator   func(map[string]QueryResultInfo) error
+}
+
+func (o MultiCheck) Validate() error {
+	return o.validator(o.checksData())
+}
+
+func (o MultiCheck) Query() (data QueryResult, err error) {
+	return
+}
+
+func (o MultiCheck) Info() string {
+	return o.info
+}
+
+func (o MultiCheck) checksData() (checksData map[string]QueryResultInfo) {
+	checksData = make(map[string]QueryResultInfo)
+	for _, check := range o.checks {
+		data, err := check.Query()
+
+		if err == nil {
+			if o.pattern != nil {
+				matches := o.pattern.FindSubmatch(data)
+				if matches == nil {
+					checksData[check.Info()] = QueryResultInfo{err: errors.New("No match")}
+				} else if len(matches) > 0 {
+					checksData[check.Info()] = QueryResultInfo{result: matches[1]}
+					//log.Debug("%v = %v", check.Info(), string(matches[1]))
+				} else {
+					checksData[check.Info()] = QueryResultInfo{result: matches[0]}
+					//log.Debug("%v = %v", check.Info(), string(matches[0]))
+				}
+			} else {
+				checksData[check.Info()] = QueryResultInfo{result: data}
+			}
+		} else if !o.onlyRunning {
+			checksData[check.Info()] = QueryResultInfo{result: data, err: err}
+		}
+	}
+	return
+}
